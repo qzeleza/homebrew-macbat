2c1426e813531535afc2b84000eaa680bdb72cda
zeleza <mail@zeleza.ru>
2025-10-03 06:47:22 +0300

    macbat v2.1.23 (bin)

--- Formula/macbat.rb
+++ Formula/macbat.rb
@@ -1,15 +1,15 @@
 class Macbat < Formula
   desc "Утилита мониторинга аккумулятора (binary)"
   homepage "https://github.com/qzeleza/macbat"
-  version "v2.1.22"
+  version "v2.1.23"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/qzeleza/macbat/releases/download/v2.1.22/macbat-darwin-arm64.tar.gz"
-      sha256 "feb9d526871738c3fa5ada58caf57afb4bd95c46d90186ba41fa2afe55917126"
+      url "https://github.com/qzeleza/macbat/releases/download/v2.1.23/macbat-darwin-arm64.tar.gz"
+      sha256 "3fb8efe86ab4a6e96b82920714ee15634c8e1adb52cdb2f18810a73075f70ce4"
     else
-      url "https://github.com/qzeleza/macbat/releases/download/v2.1.22/macbat-darwin-amd64.tar.gz"
-      sha256 "67dd6b7333828c067130a785c8a14654ef560782ee7b1bc89cba861b391e87df"
+      url "https://github.com/qzeleza/macbat/releases/download/v2.1.23/macbat-darwin-amd64.tar.gz"
+      sha256 "89152e9f437c02e3dd0eb1e4ca34a5f4d733bdaa25b0b977260b7b038a6c4164"
     end
   end
 
